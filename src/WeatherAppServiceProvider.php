efeb67cd8861b9b9a51447883b40af96329ec0a5
oldman10000 <patrickoldman1993@gmail.com>
2022-09-10 16:33:26 +0100

    Load routes, migrations and views in service provider

--- src/WeatherAppServiceProvider.php
+++ src/WeatherAppServiceProvider.php
@@ -40,34 +40,10 @@ class WeatherAppServiceProvider extends ServiceProvider
             $this->commands([CreateIpAddresses::class]);
         }
 
-        if (!class_exists('CreateIpAddressTable')) {
-            $this->publishes(
-                [
-                    __DIR__ .
-                    '/../database/migrations/create_allowed_ip_addresses_table.php.stub' => database_path(
-                        'migrations/' .
-                            date('Y_m_d_His', time()) .
-                            '_create_allowed_ip_addresses_table.php'
-                    ),
-                    // you can add any number of migrations here
-                ],
-                'migrations'
-            );
-        }
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
-        if (!class_exists('CreateWeatherReportTable')) {
-            $this->publishes(
-                [
-                    __DIR__ .
-                    '/../database/migrations/create_weather_report_table.php.stub' => database_path(
-                        'migrations/' .
-                            date('Y_m_d_His', time()) .
-                            '_create_weather_report_table.php'
-                    ),
-                    // you can add any number of migrations here
-                ],
-                'migrations'
-            );
-        }
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->loadViewsFrom(__DIR__ . '/../views', 'weather-app');
     }
 }
